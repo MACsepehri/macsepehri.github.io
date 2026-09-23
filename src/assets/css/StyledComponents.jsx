eb83aeadf02f9ab2bc738d9972579de02d8ce2aa
MACsepehri <macsepehri@gmail.com>
2026-09-23 20:24:08 +0330

    Fixed assets bugs

--- src/assets/css/StyledComponents.jsx
+++ src/assets/css/StyledComponents.jsx
@@ -3,7 +3,7 @@ import styled, { createGlobalStyle } from "styled-components"
 export const GlobalStyle = createGlobalStyle`
     @font-face {
         font-family: 'IranYekan';
-        src: url('../font/IRANYekanX-Light.woff');
+        src: url('./src/assets/font/IRANYekanX-Light.woff');
     }
     * {
         font-family: 'IranYekan';
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { GlobalStyle } from '../public/css/StyledComponents.jsx'
+import { GlobalStyle } from './assets/css/StyledComponents.jsx'
+
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <GlobalStyle />
-        <App />
+        <BrowserRouter>
+            <GlobalStyle />
+            <App />
+        </BrowserRouter>
     </StrictMode>
 );
