6eba76e418acacb099ca5b654c1e37df5332b197
MACsepehri <macsepehri@gmail.com>
2026-09-24 20:17:01 +0330

    Rebote init

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer style={{position:'absolute',marginTop:'200vh',right:'50%',transform:'translateX(50%)'}}>
-            <p style={{color:'#cfe2ff'}}>تمامی سایت توسط <b>آرین سپهری مهر</b> ساخته شده است ©</p>
-        </footer>
-    )
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import './index.css'
 import App from './App.jsx'
-import { GlobalStyle } from '../public/css/StyledComponents.jsx'
-
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter>
-            <GlobalStyle />
-            <App />
-        </BrowserRouter>
-    </StrictMode>
-);
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
