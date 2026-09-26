b6214bfdc272fe019652db51608fafaf6e83e277
MACsepehri <macsepehri@gmail.com>
2026-09-26 16:57:18 +0330

    Added a small login UI

--- public/style/StyleComponents.jsx
+++ public/style/StyleComponents.jsx
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         font-family: 'IranYekan';
         src: url('font/font.woff');
     }
-    * { font-family: 'IranYekan' }
+    * { font-family: 'IranYekan'; color: white; transition: all 300ms ease-in-out; }
     body {
         background-color: #002240;
     }
@@ -19,6 +19,35 @@ export const Section = styled.div`
     height: calc(100vh - 100px);
     background: #11385a;
     margin-top: 30px;
-    border-radius: 10px;
     border-bottom: 2px solid #fff;
+    text-align: center;
+`
+export const LoginSection = styled.div`
+    position: absolute;
+    right: 50%;
+    top: 50%;
+    transform: translate(50%,-50%);
+`
+export const InputBox = styled.input`
+    padding: 10px;
+    width: 140px;
+    border: none;
+    outline: none;
+    background: none;
+    border-bottom: 2px solid #041625;
+    font-size: 15px;
+`
+export const SubmitLoginButton = styled.button`
+    width: 160px;
+    padding: 15px;
+    background: #041625;
+    border: 3px solid #041625;
+    font-size: 17px;
+    margin-top: 10px;
+    cursor: pointer;
+    &:hover {
+        background: none;
+        color: #041625;
+        font-weight: 600;
+    }
 `
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,27 @@
-import { BaseSection, Section } from "../../public/style/StyleComponents";
+import { BaseSection, Section, LoginSection, InputBox, SubmitLoginButton } from "../../public/style/StyleComponents";
+
+function render(isLogin) {
+    if (isLogin===false) {
+
+        // Render Login
+        return (
+            <LoginSection>
+                <h2>برای شروع وارد شوید.</h2><br />
+                <div>
+                    <InputBox type="text" placeholder="نام کاربری" /><br />
+                    <SubmitLoginButton>ورود</SubmitLoginButton>
+                </div>
+            </LoginSection>
+        )
+
+    } else {}
+}
 
 export default function Box({isLogin}) {
     return (
         <BaseSection>
             <Section>
-                
+                {render(isLogin)}
             </Section>
         </BaseSection>
     )
